274dcf5b0cddfb41d670eb128b2ce92242faa3b4
Ken <knetngyn@gmail.com>
2022-08-26 13:13:07 -0500

    Create insert and delete functions

--- bst.js
+++ bst.js
@@ -5,6 +5,15 @@ const bstNode = (value = null, left = null, right = null) => {
 tree = (array = []) => {
   let root = buildTree(array);
 
+  function getMinValue(node) {
+    let minValue = node.value;
+    while (node.left !== null) {
+      minValue = node.left.value;
+      node = node.left;
+    }
+    return minValue;
+  }
+
   function buildTree(arr) {
     if (arr.length === 0) return null;
 
@@ -17,5 +26,32 @@ tree = (array = []) => {
       buildTree(sorted.slice(mid + 1))
     );
     return root;
+  }
+
+  function insertNode(value, node = root) {
+    if (node === null) return bstNode(value);
+    if (node.value > value) {
+      insertNode(value, node.left);
+    }
+    if (node.value < value) {
+      insertNode(value, node.right);
+    }
+    return node;
+  }
+
+  function deleteNode(value, node = root) {
+    if (node === null) return node;
+    if (node.value < value) {
+      node.right = deleteNode(value, node.right);
+    } else if (node.value > value) {
+      node.left = deleteNode(value, node.left);
+    } else {
+      if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      }
+    }
+    return node;
   }
 };
